d8bfdbce98183a2398b06558882b264dcaff252b
momcilo.spasenovski@hotmail.com <momcilo.spasenovski@hotmail.com>
2022-06-16 22:47:48 +0200

    Initial push. Created menu and starting UI

---
+++ Projekat - Grupa 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat___Grupa_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.CursorVisible = false;
+            Console.WriteLine();
+
+
+            myMenu meni = new myMenu(new string[] { "Pokreni igru", "Pravila", "Istorija igara", "Napusti igru" }, @"
+          .         .                                                                                                                          
+         ,8.       ,8.           ,o888888o.                8 8888           8 888888888o   8 888888888o.      ,o888888o.                8 8888 
+        ,888.     ,888.       . 8888     `88.              8 8888           8 8888    `88. 8 8888    `88.  . 8888     `88.              8 8888 
+       .`8888.   .`8888.     ,8 8888       `8b             8 8888           8 8888     `88 8 8888     `88 ,8 8888       `8b             8 8888 
+      ,8.`8888. ,8.`8888.    88 8888        `8b            8 8888           8 8888     ,88 8 8888     ,88 88 8888        `8b            8 8888 
+     ,8'8.`8888,8^8.`8888.   88 8888         88            8 8888           8 8888.   ,88' 8 8888.   ,88' 88 8888         88            8 8888 
+    ,8' `8.`8888' `8.`8888.  88 8888         88            8 8888           8 8888888888   8 888888888P'  88 8888         88            8 8888 
+   ,8'   `8.`88'   `8.`8888. 88 8888        ,8P 88.        8 8888           8 8888    `88. 8 8888`8b      88 8888        ,8P 88.        8 8888 
+  ,8'     `8.`'     `8.`8888.`8 8888       ,8P  `88.       8 888'           8 8888      88 8 8888 `8b.    `8 8888       ,8P  `88.       8 888' 
+ ,8'       `8        `8.`8888.` 8888     ,88'     `88o.    8 88'            8 8888    ,88' 8 8888   `8b.   ` 8888     ,88'     `88o.    8 88'  
+,8'         `         `8.`8888.  `8888888P'         `Y888888 '              8 888888888P   8 8888     `88.    `8888888P'         `Y888888 '    
+
+
+
+                                           DobrodoÅ¡li u igru Moj Broj! Odaberite opciju iz menija!
+");
+            int index = meni.Start();
+            while (index != 3)
+            {
+                index = meni.Start();
+            }
+        }
+    }
+}
---
+++ Projekat - Grupa 2/myMenu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat___Grupa_2
+{
+    internal class myMenu
+    {
+        private int index;
+        private string[] opcije;
+        private string naslov;
+        public myMenu(string[] opcije, string naslov)
+        {
+            this.opcije = opcije;
+            this.naslov = naslov;
+            index = 0;
+        }
+        private void Ispis()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(naslov);
+            Console.ResetColor();
+            Console.CursorVisible = false;
+
+            for (int i = 0; i < opcije.Length; i++)
+            {
+                if (i == index)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write((i + 1).ToString() + ". ");
+                Console.ResetColor();
+                Console.WriteLine(opcije[i]);
+            }
+            Console.ResetColor();
+        }
+        public int Start()
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Clear();
+                Console.CursorVisible = false;
+                Ispis();
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.UpArrow && index > 0) index--;
+                else if (key.Key == ConsoleKey.DownArrow && index < opcije.Length - 1) index++;
+            } while (key.Key != ConsoleKey.Enter);
+            return index;
+        }
+    }
+}
